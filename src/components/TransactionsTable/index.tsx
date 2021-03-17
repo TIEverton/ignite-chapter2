4cdb3d74e1fda701b69c882f63570de01a3f668e
Everton Pinheiro <evertonpinheiroti@hotmail.com>
2021-03-17 17:14:40 -0300

    :sparkles: Listing transactions and seeds

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { api } from '../../services/api';
 import { Container } from './styles';
 
+interface Transactions {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransaction] = useState<Transactions[]>([]);
+
   useEffect(() => {
     api.get('/transactions')
-      .then(response => console.log(response.data))
+      .then(response => setTransaction(response.data.transactions))
   }, [])
 
   return (
@@ -21,18 +32,16 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td className="withdraw">- R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2022</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2022</td>
-          </tr>
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>{transaction.amount}</td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createAt}</td>
+              </tr>
+            )
+          })}
         </tbody>
 
       </table>
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,29 @@ createServer({
   models: {
     transaction: Model,
   },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [{
+        id: 1,
+        title: 'Freelancer Website',
+        type: 'deposit',
+        category: 'Dev',
+        amount: 6000,
+        createAt: new Date('2021-02-12 09:00:00'),
+      },
+      {
+        id: 2,
+        title: 'Compra de bicicleta',
+        type: 'withdraw',
+        category: 'Compra',
+        amount: 1600,
+        createAt: new Date('2021-02-14 11:00:00'),
+      }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
 
